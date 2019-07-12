939e7f17b6345e5f89058c03e93a90400e4993ef
nghialt9 <lamtrongnghia1990@gmail.com>
2019-07-12 16:21:48 +0700

    edit choice commentBox

--- main.js
+++ main.js
@@ -6,13 +6,14 @@ import { Provider } from 'react-redux';
 import todoApp from './src/reducers/reducers';
 import App from './App';
 import Video from './src/demoPlayVideo/video';
+import CommentBox from './src/demoPlayVideo/commentBox';
 
 let store = createStore(todoApp)
 let rootElement = document.getElementById('app')
 
 render(
     <Provider store = {store}>
-        <Video />
+        <CommentBox url='http://localhost:3000/comments' pollInterval='700' />
     </Provider>,
     rootElement
 )
